8bb6c430210678d8f13aa218040e364914290129
AvirukBasak <avirukbasak@gmail.com>
2025-11-02 13:55:05 +0530

    rm unused method

--- backend/backend/src/main/java/com/studentmanagesystem/backend/authentication/security/SecurityConfig.java
+++ backend/backend/src/main/java/com/studentmanagesystem/backend/authentication/security/SecurityConfig.java
@@ -7,8 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -52,11 +50,11 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        // NOTE: we can safely remove this method
-        return config.getAuthenticationManager();
-    }
+    // @Bean
+    // AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    //     // NOTE: we can safely remove this method
+    //     return config.getAuthenticationManager();
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
