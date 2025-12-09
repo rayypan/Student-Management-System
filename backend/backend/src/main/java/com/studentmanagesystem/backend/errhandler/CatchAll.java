1017edb89b14f9ae951e3ebd5e55a76934228e2c
AvirukBasak <avirukbasak@gmail.com>
2025-12-09 21:18:56 +0530

    add catch for all resource not exceptions

--- backend/backend/src/main/java/com/studentmanagesystem/backend/errhandler/CatchAll.java
+++ backend/backend/src/main/java/com/studentmanagesystem/backend/errhandler/CatchAll.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.studentmanagesystem.backend.dtos.GenericMessage;
 import com.studentmanagesystem.backend.errors.UserMessageException;
@@ -15,7 +16,6 @@ public class CatchAll {
     // Catch for custom thrown UserMessageException
     @ExceptionHandler(UserMessageException.class)
     public ResponseEntity<GenericMessage> handleUserMessageException(UserMessageException ex) {
-        System.err.println(ex);
         GenericMessage gm = new GenericMessage(ex.getStatus(), ex.getMessage());
         return ResponseEntity.status(ex.getStatus()).body(gm);
     }
@@ -23,15 +23,20 @@ public class CatchAll {
     // Catch for any JwtException
     @ExceptionHandler(JwtException.class)
     public ResponseEntity<GenericMessage> handleJwtException(JwtException ex) {
-        System.err.println(ex);
         GenericMessage gm = new GenericMessage(HttpStatus.UNAUTHORIZED.value(), "Invalid credentials");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(gm);
+    }
+
+    // Catch for any NoResourceFoundException
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<GenericMessage> handleJwtException(NoResourceFoundException ex) {
+        GenericMessage gm = new GenericMessage(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(gm);
     }
 
     // Catch for any IllegalArgumentException
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<GenericMessage> handleIllegalArgumentException(IllegalArgumentException ex) {
-        System.err.println(ex);
         GenericMessage gm = new GenericMessage(HttpStatus.BAD_REQUEST.value(), "Bad Request");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(gm);
     }
